ddf9cc9d04b3ef1d9250c733ea1433f0c84d38be
Hafizulakbar <hafizul.ab@mhs.unsyiah.ac.id>
2021-12-07 22:27:37 +0700

    Project_RKPL
    Aplikasi web Dentalk

---
+++ Dentalk laravel/resources/views/index.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="../assets/css/styles-sabri.css">
+
+    <title>Dentalk</title>
+
+</head>
+
+<body background="../assets/foto/background.png">
+
+    <!-- SCROLL TOP -->
+    <a href="#" class="scrolltop" id="scroll-top">
+        <i class='bx bx-chevron-up scrolltop__icon'></i>
+    </a>
+
+    <!-- HEADER -->
+    <header class="l-header" id="header">
+        <nav class="nav bd-container">
+
+            <div class="nav__menu" id="nav-menu">
+                <ul class="nav__list">
+                    <li class="nav__item"><a href="#home" class="nav__link active-link">Home</a></li>
+                    <li class="nav__item"><a href="#whatis" class="nav__link">What Is Dentalk?</a></li>
+
+                    <li><i class='bx  change-theme' id="theme-button"></i></li>
+                </ul>
+            </div>
+
+            <div class="nav__toggle" id="nav-toggle">
+                <i class='bx bx-menu'></i>
+            </div>
+        </nav>
+    </header>
+
+    <main class="l-main">
+        <!-- HOME -->
+        <section class="home" id="home">
+            <br><br><br>
+            <div class="home__container  bd-grid">
+                <div class="home__data">
+                    <h1 class="home__title">Dentalk</h1>
+                    <h2 class="home__subtitle">Tempat untuk menulis Keluhanmu</h2>
+                    <a href="/login" class="button">Masuk</a>
+                </div>
+
+                <img src="../assets/foto/Saly-16.png" alt="" class="home__img">
+            </div>
+        </section>
+
+        <!-- ABOUT -->
+        <section class="about section bd-container" id="whatis">
+
+            <div class="about__container  bd-grid">
+                <div class="about__data">
+                    <span class="section-subtitle about__initial">What Is Dentalk?</span>
+                    <h2 class="section-title about__initial">Dentalk adalah situs web dimana kamu bisa memberikan seputar keluhanmu kepada dokter melewati
+                        sebuah aplikasi web,yang nantinya dokter akan menanggapi keluhanmu itu.
+                    </h2>
+                </div>
+
+                <img src="../assets/foto/Saly-14.png" alt="" class="about__img">
+                <br>
+            </div>
+        </section>
+
+
+
+      
+</body>
+
+</html>
